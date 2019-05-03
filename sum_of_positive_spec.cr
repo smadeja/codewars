d9b28efa5498ff0ddb347aea2192c9ae7f64b68a
Szymon Madeja <smadeja1@gmail.com>
2019-05-03 21:45:59 +0200

    Fix a typo

--- sum_of_positive_spec.cr
+++ sum_of_positive_spec.cr
@@ -2,7 +2,7 @@ require "spec"
 require "./sum_of_positive"
 
 describe "#positive_sum" do
-  it "correctly sum all the positive numbers in the array" do
+  it "correctly sums all the positive numbers in the array" do
     positive_sum([1, 2, 3, 4, 5]).should eq 15
     positive_sum([1, -2, 3, 4, 5]).should eq 13
     positive_sum([-1, 2, 3, 4, -5]).should eq 9
